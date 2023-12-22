def742ebe99649f6c254ef2faa26f09027023c7e
Vũ Kiên Trường <91974329+KTruong9303@users.noreply.github.com>
2023-12-22 09:48:44 +0700

    Update main.py

--- main.py
+++ main.py
@@ -8,5 +8,23 @@ class_list = {'0': 'Setosa', '1': 'Versicolor', '2': 'Virginica'}
 
 st.title('Iris classification based on sepal and petal size')
 
-image = Image.open('iris_flower.jpg')
+image = Image.open('iris.jpg')
 st.image(image)
+
+input = open('lrc_iris.pkl', 'rb')
+model = pkl.load(input)
+
+st.header('Choose the size of sepal and petal size')
+
+sepal_length = st.slider('Sepal length (cm)', 0.0, 20.0, 3, 0.2)
+sepal_width = st.slider('Sepal width (cm)', 0.0, 20.0, 2.7, 0.1)
+petal_length = st.slider('Petal length (cm)', 0.0, 20.0, 2.7, 0.2)
+petal_width = st.slider('Petal width (cm)', 0.0, 20.0, 2.7, 0.1)
+
+  if st.button('Predict'):
+        feature_vector = np.array([sepal_length, sepal_width, petal_length, petal_width])
+        feature_vector = feature_vector.reshape(1, -1)
+        label = str((model.predict(feature_vector))[0])
+
+        st.header('Result')
+        st.text(class_list[label])
