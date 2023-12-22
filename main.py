a5ad29265148e24404c5ec03ddf5008c9b71e6d5
Vũ Kiên Trường <91974329+KTruong9303@users.noreply.github.com>
2023-12-22 09:44:50 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
+import streamlit as st
+import pickle as pkl
+from PIL import Image
+import numpy as np
 
+class_list = {'0': 'Setosa', '1': 'Versicolor', '2': 'Virginica'}
+
+
+st.title('Iris classification based on sepal and petal size')
+
+image = Image.open('iris_flower.jpg')
+st.image(image)
